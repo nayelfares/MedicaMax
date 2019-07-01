7167de6a61307706f24c9af463620d0daa561af7
AboObe <alaa.ba6ha@gmail.com>
2019-07-01 13:20:35 +0300

    tree dd and style

--- public/assets/js/ckeditor/config.js
+++ public/assets/js/ckeditor/config.js
@@ -3,6 +3,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+              config.contentsCss = './assets/js/ckeditor/CustomFonts/fonts.css';
+              config.font_names =  'ArefRuqaa-Regular; ArefRuqaa-Regular;'+config.font_names;
               config.extraPlugins = 'sharedspace,lineheight,video,youtube,allowsave';
               config.removePlugins = 'resize';
               config.height = 140
@@ -12,12 +14,57 @@ CKEDITOR.editorConfig = function( config ) {
               config.sharedSpaces = {
                 top: 'top',
                 bottom: 'bottom'
-              }; 
-	config.keystrokes = [
-    [ CKEDITOR.CTRL + 83, 'save' ]                       // CTRL + S
+              };
+
+
+
+              config.keystrokes = [
+    [ CKEDITOR.CTRL + 83, 'save' ],  // CTRL + S
+    [CKEDITOR.CTRL + 74, 'justifyblock'],                       
 ];
 	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode','Save', 'document', 'doctools' ] },
+		{ name: 'document', groups: [ 'mode', 'Save', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Print,NewPage,HiddenField,ImageButton,Button,Select,Textarea,TextField,Radio,Checkbox';
+};
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+              config.contentsCss = './assets/js/ckeditor/CustomFonts/fonts.css';
+              config.font_names =  'ArefRuqaa-Regular; ArefRuqaa-Regular;'+config.font_names;
+              config.extraPlugins = 'sharedspace,lineheight,video,youtube,allowsave';
+              config.removePlugins = 'resize';
+              config.height = 140
+			 // config.htmlEncodeOutput =false;
+			//  config.entries=false;
+              config.enterMode = CKEDITOR.ENTER_BR;
+              config.sharedSpaces = {
+                top: 'top',
+                bottom: 'bottom'
+              };
+
+
+
+              config.keystrokes = [
+    [ CKEDITOR.CTRL + 83, 'save' ],  // CTRL + S
+    [CKEDITOR.CTRL + 74, 'justifyblock'],                       
+];
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'Save', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
